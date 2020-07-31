9bbb9314a6de1bc86338cc82c5378fb40fb075e2
Yaroslav <yaroslavsoldat2@gmail.com>
2020-07-31 12:14:33 +0300

    psch

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = config.get('port') || 5000;
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/links.routes'));
+app.use('/t', require('./routes/redirect.routes'))
 
 async function start() {
     try {
