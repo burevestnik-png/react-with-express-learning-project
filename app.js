3e9f57121d7f1a781085c94ab69f2fed1e7cf91e
Yaroslav <yaroslavsoldat2@gmail.com>
2020-07-31 12:21:42 +0300

    psch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const PORT = config.get('port') || 5000;
@@ -9,6 +10,14 @@ app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/links.routes'));
 app.use('/t', require('./routes/redirect.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 async function start() {
     try {
